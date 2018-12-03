63fec0d9ebd8654ea0113d6409fe8b5e4f6b51ad
JesseleDurana <jesseleadt@gmail.com>
2018-12-02 21:27:32 -0400

    change hour of job

--- jobs/notificaciones.js
+++ jobs/notificaciones.js
@@ -2,8 +2,8 @@ const schedule = require('node-schedule');
 const notificacionesController = require("../controllers/notificaciones");
 
 function checkEndOfLife() {
-    var j = schedule.scheduleJob('fin_vida_util_job', '5 * * * * *', function(){
-        console.log('The answer to life, the universe, and everything!');
+    var j = schedule.scheduleJob('fin_vida_util_job', '* * 0 * *', function(){
+        console.log('checkEndOfLife');
         notificacionesController.getListActivos();
     });
 }
