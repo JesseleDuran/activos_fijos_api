20d94a2fe95cc8a3a58ee92423d37c3474f14d7b
JesseleDurana <jesseleadt@gmail.com>
2019-02-11 23:15:17 -0400

    fix depreciacion report

--- controllers/reportes.js
+++ controllers/reportes.js
@@ -12,8 +12,9 @@ async function getList(params) {
         pool.end();
         const activos = allActivos.rows;
         for (let activo of activos) {
+            activo.fin_vida_util = getFinVidaUtil(activo.fecha_compra, activo.vida_util_meses);
             activo.depreciacion_por_mes = getDepreciacionMensual(activo.costo_unitario, activo.vida_util_meses);
-            activo.meses_depreciados = getMesesDepreciados(activo.created_at, params.report_date);
+            activo.meses_depreciados = getMesesDepreciados(activo.fecha_compra, params.report_date);
             activo.depreciacion_acumulada_meses = getDepreciacionAcumulada(activo.depreciacion_por_mes, activo.meses_depreciados);
             activo.valor_neto = getValorNeto(activo.costo_unitario, activo.depreciacion_acumulada_meses);
             
@@ -25,12 +26,16 @@ async function getList(params) {
     }
 }
 
+function getFinVidaUtil(fechaCompra, vidaUtilMeses) {
+    return moment(fechaCompra).add(vidaUtilMeses, 'months')
+}
+
 function getDepreciacionMensual(costo, vidaUtilMeses) {
     return safeDivision(costo, vidaUtilMeses);
 }
 
 function getDepreciacionAcumulada(depreciacionMensual, mesesDepreciados) {
-    return depreciacionMensual * mesesDepreciados;
+    return (depreciacionMensual * mesesDepreciados).toFixed(2);
 }
 
 function getMesesDepreciados(activoFechaIngreso, reportDate) {
@@ -40,11 +45,11 @@ function getMesesDepreciados(activoFechaIngreso, reportDate) {
 }
 
 function getValorNeto(costo, depreciacionAcumulada) {
-    return costo - depreciacionAcumulada;
+    return (costo - depreciacionAcumulada).toFixed(2);
 }
 
 function safeDivision(numerador, denominador) {
-    return denominador !== 0 ? numerador / denominador : 0;
+    return denominador !== 0 ? (numerador / denominador).toFixed(2) : 0;
 }
 
 module.exports = {
