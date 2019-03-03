4664eaeb3ed15608a520fb6039078ea09c6c0e73
JesseleDurana <jesseleadt@gmail.com>
2019-03-03 18:56:30 -0400

    create some swagger doc, delete non used models

--- routes/activos.js
+++ routes/activos.js
@@ -16,7 +16,7 @@ secureRouter.use(auth.jwt());
  *     produces:
  *       - application/json
  *     parameters:
- *       - name: Activo
+ *       - name: body
  *         in: body
  *         required: true
  *         schema:
@@ -82,6 +82,48 @@ secureRouter.post(
     ])
 );
 
+/**
+ * @swagger
+ * /activos/{id}:
+ *   put:
+ *     description: Actualizar cualquier atributo de un activo.
+ *     tags:
+ *      - Activos
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Número de activo que se ve a actualizar.
+ *         in: path
+ *         required: true
+ *       - name: body
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 description: Nombre del atributo que se quiere actualizar.
+ *                 type: string.
+ *               value:
+ *                 description: Valor con el que se quiere actualizar el atributo.
+ *                 type: string.
+ *     responses:
+ *       200:
+ *         description: El activo fue actualizado exitosamente.
+ *         schema:
+ *           $ref: '#/definitions/Activo'
+ *       400:
+ *         description: Hubo algún problema en los parámetros ingresados.
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ *       401:
+ *         description: No autorizado.
+ *         type: string
+ * 
+ */
 secureRouter.put(
     "/:id",
     handler(activo.update, (req, res, next) => [
@@ -99,10 +141,15 @@ secureRouter.put(
  *      - Activos
  *     produces:
  *      - application/json
- *     security: []
  *     responses:
  *       200:
  *         description: Las clasificaciones fueron retornadas exitosamente.
+ *         type: array
+ *         items:
+ *           type: string
+ *       401:
+ *         description: No autorizado.
+ *         type: string
  */
 secureRouter.get(
     "/clasificaciones",
@@ -118,16 +165,46 @@ secureRouter.get(
  *      - Activos
  *     produces:
  *      - application/json
- *     security: []
  *     responses:
  *       200:
  *         description: Las marcas fueron enviadas exitosamente.
+ *         type: array
+ *         items:
+ *           type: string
+ *       401:
+ *         description: No autorizado.
+ *         type: string
  */
 secureRouter.get(
     "/marcas",
     handler(activo.getMarcas, (req, res, next) => [])
 );
 
+/**
+ * @swagger
+ * /activos/movimiento:
+ *   get:
+ *     description: Retorna los activos disponible para el tipo de movimiento que se pase.
+ *     tags:
+ *      - Activos
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *       - name: query
+ *         description: Tipo de movimiento.
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Los activos fueron retornados exitosamente.
+ *         type: array
+ *         items:
+ *             $ref: '#/definitions/ActivoConMovimiento'
+ *       401:
+ *         description: No autorizado.
+ *         type: string
+ */
 secureRouter.get(
     "/movimiento",
     handler(activo.getListByMovement, (req, res, next) => [req.query])
@@ -142,10 +219,12 @@ secureRouter.get(
  *      - Activos
  *     produces:
  *      - application/json
- *     security: []
  *     responses:
  *       200:
  *         description: Los activos fueron retornados exitosamente.
+ *       401:
+ *         description: No autorizado.
+ *         type: string
  */
 secureRouter.get("/", handler(activo.getList, (req, res, next) => [req.query]));
 
