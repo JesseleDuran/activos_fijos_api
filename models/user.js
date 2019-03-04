c2d71a8704355c89cbd7da75cce78b2ce33f2da7
JesseleDurana <jesseleadt@gmail.com>
2019-03-03 21:38:22 -0400

    doc for user and divide activos models

---
+++ models/user.js
@@ -0,0 +1,34 @@
+  /**
+ * @swagger
+ * definitions:
+ *   User:
+ *     type: object
+ *     properties:
+ *       codemp:
+ *         type: string
+ *       codusu:
+ *         type: string
+ *       cedusu:
+ *         type: string
+ *       nomusu:
+ *         type: string
+ *       apeusu:
+ *         type: string
+ *       pwdusu:
+ *         type: string
+ *       telusu:
+ *         type: string
+ *       nota:
+ *         type: string
+ *       actusu:
+ *         type: integer
+ *       blkusu:
+ *         type: integer
+ *       admusu:
+ *         type: integer
+ *       ultingusu:
+ *         type: string
+ *         format: date-time
+ *       fotousu:
+ *         type: string
+ */
--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,48 @@
 const user = require("../controllers/user");
 const express = require("express");
 const router = express.Router();
-const secureRouter = express.Router();
 const handler = require("../utils/ControllerHandler");
 const auth = require("../auth");
 
+/**
+ * @swagger
+ * /user/auth:
+ *   post:
+ *     description: Inicio de sesión.
+ *     tags:
+ *      - User
+ *     produces:
+ *      - application/json
+ *     security: []
+ *     parameters:
+ *       - name: user
+ *         in:  body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string
+ *             password:
+ *               description: Contraseña del user, usada para autenticar el user.
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Información del user y token de autorización.
+ *         schema:
+ *            type: object
+ *            properties:
+ *              token:
+ *                type: string
+ *              user:
+ *                $ref: '#/definitions/User'
+ *			 401:
+ *         description: Usuario no autorizado, significa que los credenciales no fueron válidos.
+ *         type: string
+ */
 router.post(
     "/auth",
     auth.user,
     handler(user.auth, (req, res, next) => [req.user])
 );
-
 
 module.exports = router;
